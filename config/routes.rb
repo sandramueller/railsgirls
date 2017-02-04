0dc89ff336a40259c82dbbe2d81bc876b6e8d260
Sandra Müller <mue.sandy@gmail.com>
2017-02-04 15:02:49 +0100

    displayed the name instead of email on log in information

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   resources :comments
   get 'pages/info'
 
